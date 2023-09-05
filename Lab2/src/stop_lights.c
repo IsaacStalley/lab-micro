658b0e383e1e94e607eed21c5151ac71371ff6a5
Isaac Stalley <isaacstalley@gmail.com>
2023-09-05 11:12:32 -0600

    State machine body
    Need to add transition logic and how to tie it together with event handling

---
+++ Lab2/src/stop_lights.c
@@ -0,0 +1,48 @@
+#include <pic14/pic12f683.h>
+
+#define GREEN_STATE 0;
+#define RED_STATE 1;
+#define YELLOW_STATE 2;
+
+struct stateMachine {
+    int current_state;
+    // We can add more variables needed here.
+};
+
+void transitionToGreen(struct StateMachine sM) {
+    sM.current_state = GREEN_STATE;
+}
+
+void transitionToYellow(struct StateMachine sM) {
+    sM.current_state = YELLOW_STATE;
+}
+
+void transitionToRed(struct StateMachine sM) {
+    sM.current_state = RED_STATE;
+}
+
+void updateStateMachine(struct StateMachine sM) {
+    switch (sM.current_state) {
+        case RED_STATE:
+            transitionToGreen(sM);
+            break;
+        case YELLOW_STATE:
+            transitionToRed(sM);
+            break;
+        case GREEN_STATE:
+            transitionToYellow(sM);
+            break;
+        default:
+            break;
+    }
+}
+
+int main() {
+    struct stateMachine sM;
+    transitionToRed(sM);
+
+    while (1) {
+        updateStateMachine(sM);
+        
+    }
+}
